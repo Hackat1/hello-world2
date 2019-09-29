cfc3e0c7c00e79cc63950912e2e99e9740309d1f
Hackat1 <55863011+Hackat1@users.noreply.github.com>
2019-09-29 12:22:25 -0700

    .htaccess automation
    Added a second page with a link to it from the index page. Also added automation for .htpasswd with 2 users and .htaccess.

--- apache-automation.sh
+++ apache-automation.sh
@@ -1,5 +1,10 @@
-yum -y install httpd mod_ssl                                                          # Install apache and ssl support
-systemctl start httpd                                                                 # Start apache
-sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf                                       # Comment out welcome page 
-echo "<html><body><h1>Hi there NTI300</h1></body></html>" > /var/www/html/index.html  # Create custom welcome page
-systemctl restart httpd                                                               # Restart apache
+yum -y install httpd mod_ssl                                                                    # Install apache and ssl support
+systemctl start httpd                                                                           # Start apache
+sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf                                                 # Comment out welcome page
+mkdir /var/www/html/pages.dir                                                                   # Make new directory for additional webpages
+echo "<html><body><h1>Hi there NTI300</h1><p><a href="pages.dir/page2.html">Page 2</a></p></body></html>" > /var/www/html/index.html  # Create custom welcome page
+echo "<html><body><h1>This is page two</h1></body></html>" > /var/www/html/pages.dir/page2.html # Create page two.
+htpasswd -ci /usr/local/etc/.htpasswd Hack P@ssw0rd0!                                           # Create .htaccess with user Hack and standard password.
+htpasswd -i /usr/local/etc/.htpasswd Hack P@ssw0rd0!                                            # Create user Hack1 with standard password
+echo "AuthUserFile /usr/local/etc/.htpasswd AuthName "Page2 Access" AuthType Basic <Limit GET POST> require user Hack require user Hack1 </Limit>" > /var/www/html/pages.dir/.htaccess # Create .htaccess file
+systemctl restart httpd                                                                         # Restart apache
