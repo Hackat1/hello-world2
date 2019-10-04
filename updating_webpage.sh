c1b828079425d444427e0c05935e4a9ceaca49db
Hackat1 <55863011+Hackat1@users.noreply.github.com>
2019-10-03 19:55:54 -0700

    Update updating_webpage.sh

--- updating_webpage.sh
+++ updating_webpage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-time=$(date)
-echo "<html><body><h1>Hi there, it is $time, how are you?</h1></body></html>" > /var/www/html/pages.dir/page2.html
+time=$(date)                                                                                                        # Set variable time equal to date command
+echo "<html><body><h1>Hi there, it is $time, how are you?</h1></body></html>" > /var/www/html/pages.dir/page2.html  # Update page2 of website with new date/time
