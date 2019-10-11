a320b5bbc0901f09879e9e8c067219a1546c94c4
Hackat1 <55863011+Hackat1@users.noreply.github.com>
2019-10-10 18:48:04 -0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ -e /usr/bin/wget ]; then 
+  exit 0; 
+fi
 yum -y install wget
 wget https://raw.githubusercontent.com/Hackat1/hello-world2/master/packages.txt
 for packages in $(cat packages.txt); do
