605cd7edf1b4a9001b6b45e7df408ebbaa305711
Hackat1 <55863011+Hackat1@users.noreply.github.com>
2019-10-10 18:33:50 -0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 yum -y install wget
+wget https://raw.githubusercontent.com/Hackat1/hello-world2/master/packages.txt
 for packages in $(cat packages.txt); do
   yum -y install $packages 
 done
