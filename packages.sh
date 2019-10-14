b7dbae397af0b96d1a283fcaee4c34b1dc7db0bf
Hackat1 <55863011+Hackat1@users.noreply.github.com>
2019-10-14 12:11:27 -0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if [ -e /usr/bin/wget ]; then 
+if [ -e /usr/bin/wget ]; then
+  echo "wget already exists" | logger
   exit 0; 
 fi
 yum install -y wget
@@ -7,8 +8,8 @@ wget https://raw.githubusercontent.com/Hackat1/hello-world2/master/packages.txt
 for packages in $(cat packages.txt); do
   yum install -y $packages 
 done
-cd root
+cd /root
 git clone https://github.com/Hackat1/hello-world2.git
-cd hello-world2/
+cd /root/hello-world2
 git config --global user.name "Hackat1"
-git config --global user.email christopher.hackat1@seattlecolleges.ed
+git config --global user.email christopher.hackat1@seattlecolleges.edu
