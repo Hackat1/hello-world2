56191d023478885dcf41623766e8209fc3c35e3a
Hackat1 <55863011+Hackat1@users.noreply.github.com>
2019-10-13 21:56:35 -0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -2,12 +2,12 @@
 if [ -e /usr/bin/wget ]; then 
   exit 0; 
 fi
-yum -y install wget
+yum install -y wget
 wget https://raw.githubusercontent.com/Hackat1/hello-world2/master/packages.txt
 for packages in $(cat packages.txt); do
-  yum -y install $packages 
+  yum install -y $packages 
 done
 git clone https://github.com/Hackat1/hello-world2.git
 cd hello-world2/
 git config --global user.name "Hackat1"
-git config --global user.email christopher.hackat1@seattlecolleges.edu
+git config --global user.email christopher.hackat1@seattlecolleges.ed
