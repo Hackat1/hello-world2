a248ebfd99d66d9900810a2f1185b83fd2bfc319
Hackat1 <55863011+Hackat1@users.noreply.github.com>
2019-10-14 12:14:36 -0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-if [ -e /usr/bin/wget ]; then
-  echo "wget already exists" | logger
-  exit 0; 
-fi
-yum install -y wget
-wget https://raw.githubusercontent.com/Hackat1/hello-world2/master/packages.txt
-for packages in $(cat packages.txt); do
-  yum install -y $packages 
-done
-cd /root
-git clone https://github.com/Hackat1/hello-world2.git
-cd /root/hello-world2
-git config --global user.name "Hackat1"
-git config --global user.email christopher.hackat1@seattlecolleges.edu
+if [ -e /usr/bin/wget ]; then                                                       # Test if wget is installed
+  echo "wget already exists" | logger                                               #
+  exit 0;                                                                           # Exit if wget is installed
+fi                                                                                  #
+yum install -y wget                                                                 # Install wget
+wget https://raw.githubusercontent.com/Hackat1/hello-world2/master/packages.txt     # wget the packages.txt file from github
+for packages in $(cat packages.txt); do                                             # Install all packages in the packages.txt file
+  yum install -y $packages                                                          #
+done                                                                                #
+cd /root                                                                            # cd to the home of root
+git clone https://github.com/Hackat1/hello-world2.git                               # Clone repository from github
+cd /root/hello-world2                                                               # cd to the new directory hello-world2
+git config --global user.name "Hackat1"                                             # Set the username to Hackat1
+git config --global user.email christopher.hackat1@seattlecolleges.edu              # Set the email
